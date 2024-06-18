053a12b1de00166557a87334c3ab45c31f5389bd
Sylvester Bassey <115729068+Slybss@users.noreply.github.com>
2024-06-18 15:58:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "jenkinscd-bucket"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
   }
 }
