1e20ffed57e550d1b043baef97394212a373832c
Sylvester Bassey <115729068+Slybss@users.noreply.github.com>
2024-07-13 17:26:12 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "tubularlyses"
+    bucket         = "tubularlysis"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
-    dynamodb_table = "mrslybss-dynamo-db-table"
+    dynamodb_table = "mrslybssi-dynamo-db-table"
   }
 }
